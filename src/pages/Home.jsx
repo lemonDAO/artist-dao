7d330bb4c4b72fa6f4e98f783e34dec76db1c7a4
hiba <hiba.mashfij@gmail.com>
2021-09-06 16:14:48 +0300

    added navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Navbar from '../components/Navbar.jsx'
+
+
+export default function Home() {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+       <Navbar/>
+      </div>
+    </div>
+  )
+}
