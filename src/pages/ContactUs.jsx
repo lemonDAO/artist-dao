0e70dff12992d22886d99b78dd884a9e4da11594
hiba <hiba.mashfij@gmail.com>
2021-09-08 22:48:24 +0300

    Create ContactUs.jsx

---
+++ src/pages/ContactUs.jsx
@@ -0,0 +1,12 @@
+import ContactForm from '../components/ContactForm.jsx';
+
+
+export default function ContactUs() {
+  return (
+    <div className="bg-white">
+      <div className="relative overflow-hidden">
+       <ContactForm/>
+      </div>
+    </div>
+  )
+}
