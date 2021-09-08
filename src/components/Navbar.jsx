39e6d44ccb599b426696f3d02ba3c682587f4fc0
Agnes <mate.agnesvagyok@gmail.com>
2021-09-08 17:58:36 +0200

    the code has not been tested due to lack of time, before calling it we should test it

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,14 @@
 import { wallet, CONTRACT_ID } from "../services";
+import ContactForm from "./ContactForm";
 
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
 import logo from "../images/logo.png"
+
+
+
+
 
 const accountId = wallet.getAccountId();
 
