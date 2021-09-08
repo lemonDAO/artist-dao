63b1e1f9f38fe5945395aa3c18fb3ddd6654d2fa
hiba <hiba.mashfij@gmail.com>
2021-09-08 22:48:22 +0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,9 @@
-import { wallet, CONTRACT_ID } from "../services";
-import ContactForm from "./ContactForm";
-
+import { wallet, CONTRACT_ID } from '../services';
+import { Link } from 'react-router-dom';
 import { Fragment } from 'react';
 import { Popover, Transition } from '@headlessui/react';
 import { MenuIcon, XIcon } from '@heroicons/react/outline';
-import logo from "../images/logo.png"
-
-
-
-
+import logo from '../images/logo.png';
 
 const accountId = wallet.getAccountId();
 
@@ -22,9 +17,9 @@ const signIn = () => {
 };
 
 const navigation = [
-  { name: 'About', href: '#' },
-  { name: 'Team', href: '#' },
-  { name: 'Contact', href: '#' },
+  { name: 'About', href: '/about' },
+  { name: 'Team', href: '/team' },
+  { name: 'Contact', href: '/contact-us' }
 ];
 
 export default function Navbar() {
@@ -34,10 +29,10 @@ export default function Navbar() {
         <nav className="relative max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6" aria-label="Global">
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full md:w-auto">
-              <a href="#">
+              <Link to="/">
                 <span className="sr-only">Workflow</span>
                 <img className="h-10 w-auto sm:h-24" src={logo} alt="Project Logo" />
-              </a>
+              </Link>
               <div className="-mr-2 flex items-center md:hidden">
                 <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
@@ -47,16 +42,16 @@ export default function Navbar() {
             </div>
             <div className="hidden space-x-8 md:flex md:ml-10">
               {navigation.map(item => (
-                <a key={item.name} href={item.href} className="text-base font-medium text-black hover:text-gray-300">
+                <Link key={item.name} to={item.href} className="text-base font-medium text-black hover:text-gray-300">
                   {item.name}
-                </a>
+                </Link>
               ))}
             </div>
           </div>
           <div className="hidden md:flex md:items-center md:space-x-6">
             <p>{accountId && accountId}</p>
             <button onClick={signIn} className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md text-white bg-gray-600 hover:bg-gray-700">
-              {accountId ? "Log Out" : "Log in with NEAR"}
+              {accountId ? 'Log Out' : 'Log in with NEAR'}
             </button>
           </div>
         </nav>
