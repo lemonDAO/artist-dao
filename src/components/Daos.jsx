d3a4cd8bce1ea96621b6b92fca6ee493e9b0a18e
hiba <hiba.mashfij@gmail.com>
2021-09-10 17:07:25 +0300

    Create Daos.jsx

---
+++ src/components/Daos.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { getPolicy } from '../services';
+import SingleDAO from './SingleDAO';
+
+
+export default function Policy() {
+  const [daos, setDaos] = useState();
+
+
+
+
+  const getDaos = async () => {
+    const result = await getPolicy();
+    await setDaos(result);
+  };
+
+  useEffect(() => {
+    getDaos();
+  }, []);
+
+  return (
+    <>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3" />
+        </div>
+        <div className="relative max-w-7xl mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Explore DAOs</h2>
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Fund artists and contribute to the public good</p>
+          </div>
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            {daos &&
+              daos.map(dao => {
+                return <SingleDAO dao={dao} />;
+              })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
