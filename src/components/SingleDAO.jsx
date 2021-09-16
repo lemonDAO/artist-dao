d21d1f0d49b977bfbd89c9127aec263d0e8ed6a8
hiba <hiba.mashfij@gmail.com>
2021-09-16 12:57:21 +0300

    Update SingleDAO.jsx

--- src/components/SingleDAO.jsx
+++ src/components/SingleDAO.jsx
@@ -1,39 +1,35 @@
-
 import { Link } from 'react-router-dom';
 
-export default function Policies({ dao }) {
-  console.log(dao);
+export default function SingleDao({dao , index}) {
+  
   return (
-    <div key={dao.daoId} className="flex flex-col rounded-lg shadow-lg overflow-hidden border border-dark-gray">
+    <Link to={`/dao/${index}`} key={index} onClick={()=> localStorage.setItem("daoId", dao.daoId)}>
+    <div  className="flex flex-col rounded-lg shadow-lg overflow-hidden border border-dark-gray">
       <div className="flex-1 bg-white p-6 flex flex-col justify-between">
         <div className="flex-1">
-          <p className="text-sm font-medium text-indigo-600">
-            <Link to="#" className="hover:underline">
-              {dao.daoId}
-            </Link>
-          </p>
-          <Link to="#" className="block mt-2">
+          <p className="text-sm font-medium">{dao.daoId}</p>
+          <div className="block mt-2">
             <p className="text-xl font-semibold text-gray-900">{dao.config.name}</p>
             <p className="mt-3 text-base text-gray-500">{dao.config.purpose}</p>
-          </Link>
+          </div>
         </div>
         <div>
           Council Members
           {dao.policy.roles.map(policy => (
-            <div>
+            <div key={policy.name}>
               <p>
                 {policy.name === 'council'
                   ? policy.kind.Group.map(role => {
-                      return <span>{role}</span>;
+                      return <span key={role}>{role}</span>;
                     })
                   : ''}
               </p>
-              <p>
-              </p>
+              <p></p>
             </div>
           ))}
         </div>
       </div>
     </div>
+    </Link>
   );
 }
