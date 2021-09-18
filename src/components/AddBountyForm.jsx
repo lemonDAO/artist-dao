a40cba04b314c28ae63f1b0d62f4c2c16e0509f4
hiba <hiba.mashfij@gmail.com>
2021-09-18 14:58:15 +0300

    Update AddBountyForm.jsx

--- src/components/AddBountyForm.jsx
+++ src/components/AddBountyForm.jsx
@@ -3,9 +3,7 @@ import { useState } from 'react';
 import { addBounty } from '../services';
 
 export default function AddBountyForm() {
-
-
-  const [bountyForm, setBountyForm] = useState({ description: '', bountyDescription: '', amount: '' , times: 1, maxDeadline : "500"});
+  const [bountyForm, setBountyForm] = useState({ description: '', bountyDescription: '', amount: '', times: 1, maxDeadline: '500' });
 
   const handleChange = e => {
     setBountyForm({
@@ -16,73 +14,52 @@ export default function AddBountyForm() {
 
   const handleSubmit = (e, bountyForm) => {
     e.preventDefault();
-    const POLICY_ID = localStorage.getItem('daoId')
+    const POLICY_ID = localStorage.getItem('daoId');
     addBounty({ POLICY_ID, ...bountyForm });
   };
 
   return (
- 
-    <div className="flex items-center justify-center my-20">
-    <form>
-      <label htmlFor="description" className="mx-2">General description</label>
-      <input
-        value={bountyForm.description}
-        onChange={e => handleChange(e)} 
-        type="text"
-        name="description"
-      
-        id="description"
-        placeholder="Description"
-        className="border rounded p-1 px-3 w-40 h-11"
-      />
+    <div className="relative max-w-7xl mx-auto my-12 ">
+      <form className="flex justify-center ">
+     
+        <div className="flex flex-col justify-between mb-10 w-6/12 rounded-lg border border-dark-gray shadow-lg overflow-hidden p-6">
 
-      <label htmlFor="bountyDescription" className="mx-2">Bounty description</label>
-      <input
-       value={bountyForm.bountyDescription}
-       onChange={e => handleChange(e)} 
-        type="text"
-        name="bountyDescription"
-        id="bountyDescription"
-        placeholder="Bounty Description"
-        className="border rounded p-1 px-3 w-40 h-11"
-      />
-      <label htmlFor="amount" className="mx-2">Amount</label>
-      <input
-         value={bountyForm.amount}
-         onChange={e => handleChange(e)} 
-        type="text"
-        name="amount"
-        id="amount"
-        placeholder="Amount"
-        className="border rounded p-1 px-3 w-40 h-11"
-      />
-      <label htmlFor="times" className="mx-2">Times</label>
-      <input
-        value={bountyForm.times}
-        onChange={e => handleChange(e)} 
-        type="number"
-        name="times"
-        id="times"
-        placeholder="Times"
-        className="border rounded p-1 px-3 w-40 h-11"
-      />
-      <label htmlFor="maxDeadline" className="mx-2">Max Deadline</label>
-      <input
-        value={bountyForm.maxDeadline}
-        onChange={e => handleChange(e)} 
-        type="text"
-        name="maxDeadline"
-        id="maxDeadline"
-        placeholder="Max Deadline"
-        className="border rounded p-1 px-3 w-40 h-11"
-      />
-    </form>
-    <button
-      onClick={(e)=>handleSubmit(e,bountyForm)}
-      className="bg-indigo-500 text-white hover:bg-indigo-700 flex items-center justify-center h-11 w-32 rounded-md"
-    >
-      Add bounty
-    </button>
-  </div>
+          <div className="flex justify-between my-2">
+            <label htmlFor="description" className="">
+              General description
+            </label>
+            <textarea value={bountyForm.description} onChange={e => handleChange(e)} type="text" name="description" id="description" placeholder="Description" className="border rounded p-1 px-3 w-96 h-20" />
+          </div>
+          <div className="flex justify-between my-2">
+            <label htmlFor="bountyDescription" className="">
+              Bounty description
+            </label>
+            <textarea value={bountyForm.bountyDescription} onChange={e => handleChange(e)} type="text" name="bountyDescription" id="bountyDescription" placeholder="Bounty Description" className=" w-96 border rounded p-1 px-3  h-20" />
+          </div>
+          <div className="flex justify-between my-2">
+            <label htmlFor="amount" className="mx-2">
+              Amount
+            </label>
+            <input value={bountyForm.amount} onChange={e => handleChange(e)} type="text" name="amount" id="amount" placeholder="Amount" className=" w-96 border rounded p-1 px-3  h-11" />
+          </div>
+          <div className="flex justify-between my-2">
+            <label htmlFor="times" className="mx-2">
+              Times
+            </label>
+            <input value={bountyForm.times} onChange={e => handleChange(e)} type="number" name="times" id="times" placeholder="Times" className=" w-96 border rounded p-1 px-3  h-11" />
+          </div>
+
+          <div className="flex justify-between my-2">
+            <label htmlFor="maxDeadline" className="mx-2">
+              Max Deadline
+            </label>
+            <input value={bountyForm.maxDeadline} onChange={e => handleChange(e)} type="text" name="maxDeadline" id="maxDeadline" placeholder="Max Deadline" className="border rounded p-1 px-3 w-96 h-11" />
+          </div>
+          <button onClick={e => handleSubmit(e, bountyForm)} className="self-end text-white hover:bg-indigo-700 flex items-center justify-center h-11 w-32 px-4 mt-2 py-3 border border-transparent text-base font-medium rounded-md shadow-sm ">
+            Add bounty
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
