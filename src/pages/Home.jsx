1ab76b95846a6563507c429e6396dc7a695b0e79
hiba <hiba.mashfij@gmail.com>
2021-09-18 16:15:26 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import HeroSection from '../components/HeroSection.jsx'
 import Daos from '../components/Daos.jsx';
 import LogoClouds from "../components/LogoClouds.jsx"
+import YoutubeEmbed from "../components/Video.jsx"
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <div className="relative overflow-hidden">
        <HeroSection/>
        <LogoClouds/>
+       <YoutubeEmbed/>
        <Daos/>
       </div>
     </div>
