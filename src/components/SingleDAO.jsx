d7dbc597015cb4248668770c56c429c2bc495177
hiba <hiba.mashfij@gmail.com>
2021-09-18 14:58:02 +0300

    Update SingleDAO.jsx

--- src/components/SingleDAO.jsx
+++ src/components/SingleDAO.jsx
@@ -9,18 +9,18 @@ export default function SingleDao({dao , index}) {
         <div className="flex-1">
           <p className="text-sm font-medium">{dao.daoId}</p>
           <div className="block mt-2">
-            <p className="text-xl font-semibold text-gray-900">{dao.config.name}</p>
-            <p className="mt-3 text-base text-gray-500">{dao.config.purpose}</p>
+            <p className="text-xl font-semibold text-gray-900">Name:{dao.config.name}</p>
+            <p className="mt-3 text-base text-gray-500">Purpose: {dao.config.purpose}</p>
           </div>
         </div>
-        <div>
-          Council Members
+        <div className="mt-5">
+          Council Members: 
           {dao.policy.roles.map(policy => (
             <div key={policy.name}>
               <p>
                 {policy.name === 'council'
                   ? policy.kind.Group.map(role => {
-                      return <span key={role}>{role}</span>;
+                      return <span key={role} className="flex flex-col font-semibold">- {role}</span>;
                     })
                   : ''}
               </p>
