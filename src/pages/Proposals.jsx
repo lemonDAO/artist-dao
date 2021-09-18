e18df9169d07b4ab0a624a33ddd7815b78ba094e
hiba <hiba.mashfij@gmail.com>
2021-09-18 14:57:53 +0300

    Update Proposals.jsx

--- src/pages/Proposals.jsx
+++ src/pages/Proposals.jsx
@@ -26,62 +26,66 @@ export default function Proposals() {
 
   return (
     <>
-      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div className="absolute inset-0">
-          <div className="bg-white h-1/3 sm:h-2/3" />
-        </div>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8 ">
+    
         <div className="relative max-w-7xl mx-auto">
-          <div className="text-center">
-            <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Proposals</h2>
-            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Fund artists and contribute to the public good</p>
-          </div>
-          <AddBountyForm />
+        <h2 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-center">DAO Info</h2>
           {policy && (
-            <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-              <div className="flex-1">
-                <p className="text-sm font-medium">{policy.policy.daoId}</p>
-                <div className="block mt-2">
-                  <p className="text-xl font-semibold text-gray-900">Dao Name: {policy.config.name}</p>
-                  <p className="mt-3 text-base text-gray-500">Dao purpose: {policy.config.purpose}</p>
-                </div>
+            <div className="mt-12  flex  w-9/12 flex-col mx-auto">
+              <div className="flex justify-between rounded-lg border border-dark-gray p-4">
+                <p className="text-xl tracking-tight ">
+                  DAO ID: <span className="font-semibold">{localStorage.getItem('daoId')}</span>
+                </p>
+                <p className="text-xl">
+                  Dao Name: <span className="font-semibold">{policy.config.name}</span>
+                </p>
+                <p className=" text-xl">
+                  Dao purpose: <span className="font-semibold">{policy.config.purpose}</span>
+                </p>
               </div>
-              <div>
-                Council Members
+              <div className="flex my-5 mr-2 rounded-lg border border-dark-gray p-4">
+                <p className="text-xl mr-2 p-4">Council Members: </p>
+
                 {policy.policy.roles.map(policy => (
-                  <div key={policy.name}>
-                    <p>
-                      {policy.name === 'council'
-                        ? policy.kind.Group.map(role => {
-                            return <span key={role}>{role}</span>;
-                          })
-                        : ''}
-                    </p>
-                    <p></p>
-                  </div>
+                  <p key={policy.name} className="font-semibold text-xl flex flex-col ">
+                    {policy.name === 'council'
+                      ? policy.kind.Group.map(role => {
+                          return <span key={role}>{role}</span>;
+                        })
+                      : ''}
+                  </p>
                 ))}
               </div>
             </div>
           )}
-
-          <div className="mt-20">
+          <div className="text-center mt-10">
+            <h2 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Propose a bounty</h2>
+            <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Fund artists and contribute to the public good</p>
+          </div>
+          <AddBountyForm />
+          <h2 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-4xl text-center">Proposals</h2>
+          <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+         
             {proposals &&
               proposals.map(proposal => {
                 return (
-                  <div>
-                    <h1>Proposer: {proposal.proposer}</h1>
-                    <p>Description: {proposal.description}</p>
+                  <div className="flex flex-col rounded-lg shadow-lg overflow-hidden border border-dark-gray p-4">
+                    <h1>Proposer: <span className="font-semibold">{proposal.proposer}</span></h1>
+                    <p>Description: <span className="font-semibold">{proposal.description} </span></p>
                     {proposal.kind.AddBounty && (
+                      
                       <div>
-                        <h3>Bounty description: {proposal.kind.AddBounty.bounty.description}</h3>
-                        <p>Token: {proposal.kind.AddBounty.bounty.token || 'NEAR'}</p>
-                        <p>Amount: {proposal.kind.AddBounty.bounty.amount}</p>
-                        <p>Times: {proposal.kind.AddBounty.bounty.times}</p>
-                        <p>Max deadline: {proposal.kind.AddBounty.bounty.max_deadline}</p>
-                        <h3>Status: {proposal.status}</h3>
+                         <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">Bounty Info: </p>
+                        <h3>Bounty description: <span className="font-semibold">{proposal.kind.AddBounty.bounty.description}</span></h3>
+                        <p>Token: <span className="font-semibold">{proposal.kind.AddBounty.bounty.token || 'NEAR'}</span></p>
+                        <p>Amount: <span className="font-semibold">{proposal.kind.AddBounty.bounty.amount}</span></p>
+                        <p>Times: <span className="font-semibold">{proposal.kind.AddBounty.bounty.times}</span></p>
+                        <p>Max deadline: <span className="font-semibold">{proposal.kind.AddBounty.bounty.max_deadline}</span></p>
+                        <h3>Status: <span className="font-semibold">{proposal.status}</span></h3>
                         {/* <p>Votes: {proposal.votes}</p> */}
                         <p>
-                          Submission Date:
-                          {format(new Date(fromUnixTime(parseInt(proposal.submission_time.substring(0, 10)))), 'MMM do yyyy')}
+                          Submission Date:  
+                          <span className="font-semibold">{format(new Date(fromUnixTime(parseInt(proposal.submission_time.substring(0, 10)))), 'MMM do yyyy')}</span>
                         </p>
                       </div>
                     )}
