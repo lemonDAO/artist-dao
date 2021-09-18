d949a41b6bdbdbaac40411bb362d8407c3e80fed
hiba <hiba.mashfij@gmail.com>
2021-09-18 14:58:04 +0300

    Update LogoClouds.jsx

--- src/components/LogoClouds.jsx
+++ src/components/LogoClouds.jsx
@@ -9,7 +9,7 @@ export default function LogoClouds() {
         <h2 className="max-w-md mx-auto text-3xl font-extrabold text-dark-gray text-center lg:max-w-xl lg:text-left">
         Infrastructure for Innovation
         </h2>
-        <div className="flow-root self-center ml-20 mt-8 lg:mt-0">
+        <div className="flow-root self-center lg:ml-20 mt-8 lg:mt-0">
           <div className="-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4">
             <div className="mt-4 ml-8 flex flex-grow flex-shrink-0 justify-center lg:flex-grow-0 lg:ml-4">
               <img
